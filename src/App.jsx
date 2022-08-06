004d3688c2d811ddd1aeb61db65b379d0697878b
Ân Trần Bảo <20520377@ms.uit.edu.vn>
2022-08-06 11:44:25 +0700

    fix conflict css

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
 // import ProductDetail from './pages/browse/components/Products/ProductDetail/ProductDetail';
 // import Products from './pages/browse/components/Products/ProductList';
 // import Home from './pages/browse/Home';
-import {Owner} from './pages/owner/index';
+import { Owner } from "./pages/owner/index";
+import CheckOutUI from "./pages/checkout/UI";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* <Route path='/' element={<Home />} exact /> */}
-        <Route path='/shop'>
+        <Route path="/shop">
           {/* <Route index element={<Products />}/>
           <Route path=':name/:id' element={<ProductDetail />} /> */}
         </Route>
-        <Route path='/owner' element={<Owner />} />
+        <Route path="/owner" element={<Owner />} />
+        <Route path="/checkout/*" element={<CheckOutUI />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/checkout/UI.jsx
+++ src/pages/checkout/UI.jsx
@@ -11,6 +11,18 @@ function CheckOutUI() {
 
   return (
     <div>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous"
+      />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&family=League+Spartan&family=Roboto:wght@100;300&display=swap"
+        rel="stylesheet"
+      />
       <ApolloProvider client={client}>
         {/* <BrowserRouter> */}
         <Routes>
@@ -28,9 +40,28 @@ function CheckOutUI() {
         </Routes>
         {/* </BrowserRouter> */}
       </ApolloProvider>
-
       {/* footer section  */}
       <Footer />
+
+      <script
+        src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"
+      ></script>
+
+
+
+      
     </div>
   );
 }
