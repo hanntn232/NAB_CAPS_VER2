7e9ad66f8326975a1815a06e204f42d4fb75d95c
hanntn232 <91482056+hanntn232@users.noreply.github.com>
2022-08-08 20:59:34 +0700

    fix model customerID input

--- src/pages/browse/Home.jsx
+++ src/pages/browse/Home.jsx
@@ -7,17 +7,19 @@ import { Banner1, Banner2 } from './components/Banner/Banner';
 import Footer from '../../common/Footer/Footer';
 import NewProducts from './components/Products/NewProducts/NewProducts';
 import { InputCustomerID } from '../main-page/inputCustomerID/inputCustomerID';
+import { useGlobalState } from '../main-page/customerIdState/customerIdState';
 
 
 const Home = () => {
   const { data } = useQueryGetProductList();
   let products = data?.products.map((product) => product).reverse(); 
+  const [customerId, setCustomerId] = useGlobalState('customerID')
 
   useEffect(() => { document.title = `Home. Nike For You` }, []);
 
   return (
     <>
-      <InputCustomerID isShow={true} />
+      <InputCustomerID isShow={customerId === null ? true:false} />
       <div className='home'>
         <Header link={'Home'} />
         <Banner1 />
